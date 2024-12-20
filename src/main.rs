b8f848f639bf32298264e64b4c184c754a966a09
arghpy <subaandrei2@gmail.com>
2024-12-20 17:45:11 +0200

    fix commenting

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,24 @@ pub const BACKGROUND: u32 = 0x000000; // Black
 
 fn main() {
     let mut rng = thread_rng();
-    //let mut bubble_sort_arr = Vec::new();
+    let mut bubble_sort_arr = Vec::new();
     let mut insertion_sort_arr = Vec::new();
 
     for _ in 0..(WIDTH / BAR_WIDTH) {
         let nr = rng.gen_range(1..(HEIGHT - 5));
         // In order to not fill all screen height
-        //bubble_sort_arr.push(nr);
+        bubble_sort_arr.push(nr);
         insertion_sort_arr.push(nr);
     }
 
-    //let handle_1 = thread::spawn(move || {
-    //    bubble_sort_visualization(&mut bubble_sort_arr);
-    //});
+    let handle_1 = thread::spawn(move || {
+        bubble_sort_visualization(&mut bubble_sort_arr);
+    });
 
     let handle_2 = thread::spawn(move || {
         insertion_sort_visualization(&mut insertion_sort_arr);
     });
 
-    //handle_1.join().unwrap();
+    handle_1.join().unwrap();
     handle_2.join().unwrap();
 }
