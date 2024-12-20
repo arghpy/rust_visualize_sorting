129788b271b5aa791edc3072ff92beac23f47240
arghpy <subaandrei2@gmail.com>
2024-12-20 08:53:43 +0200

    add insertion sort

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,49 @@ use std::process::{Command, Stdio};
 // 720p, 64 elements to sort
 const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
-const BAR_WIDTH: usize = 40;
+const BAR_WIDTH: usize = 20;
 
 // Colors
 const FOREGROUND: u32 = 0xFFFFFF; // White
 const BACKGROUND: u32 = 0x000000; // Black
+
+fn insertion_sort_visualization(arr: &mut [usize]) {
+    let dir_name = "insertion_sort";
+    let _ = create_dir(dir_name);
+    let mut pixels = [0u32; WIDTH * HEIGHT];
+    let mut nr = 0;
+
+    // Set by default pixels to a bright green color
+    // because we want to color all of them and this
+    // green will indicate if we covered all pixels or not
+    pixels.fill(0x00FF00);
+    bars_array(&mut pixels, arr);
+    save_as_ppm(
+        format!("{}/round-{:0>4}.ppm", dir_name, nr).as_str(),
+        &pixels,
+    ).unwrap();
+
+    let mut i = 1;
+    let mut j;
+    while i < arr.len() {
+        j = i;
+        while j > 0 && arr[j-1] > arr[j] {
+            arr.swap(j, j - 1);
+            j -= 1;
+
+            bars_array(&mut pixels, arr);
+            save_as_ppm(
+                format!("{}/round-{:0>4}.ppm", dir_name, nr).as_str(),
+                &pixels,
+            ).unwrap();
+            nr += 1;
+        }
+        i += 1;
+    }
+    convert_to_whatsapp_compatible_video(dir_name);
+    convert_video_to_gif(dir_name);
+    let _ = remove_dir_all(dir_name);
+}
 
 fn bubble_sort_visualization(arr: &mut [usize]) {
     let dir_name = "bubble_sort";
@@ -136,4 +174,5 @@ fn main() {
         arr.push(rng.gen_range(1..(HEIGHT - 5)));
     }
     bubble_sort_visualization(&mut arr.clone());
+    insertion_sort_visualization(&mut arr.clone());
 }
